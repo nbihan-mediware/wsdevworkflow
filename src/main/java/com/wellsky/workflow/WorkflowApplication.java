5d32878c60266224d0c2f46839d6bbb1c36ec678
Nicolas Bihan <nicolas.bihan@wellsky.com>
2018-12-14 09:46:31 -0600

    Bad Code

--- src/main/java/com/wellsky/workflow/WorkflowApplication.java
+++ src/main/java/com/wellsky/workflow/WorkflowApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WorkflowApplication {
 
+    public String PASSWORD = "SECRETPASSWORD";
+
     public static void main(String[] args) {
         //Bad variable name
-        String SuperHorrible="";
+        String SuperHorrible_VarName="";
         //System out should be a critical
-        System.out.println(SuperHorrible);
+        System.out.println(SuperHorrible_VarName);
         SpringApplication.run(WorkflowApplication.class, args);
     }
 }

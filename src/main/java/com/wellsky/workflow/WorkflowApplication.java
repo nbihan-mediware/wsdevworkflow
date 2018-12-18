d11fccf4ee8073361254dee8f322ee2e8efe7b63
Nicolas Bihan <nicolas.bihan@wellsky.com>
2018-12-18 17:09:58 -0600

    Fixing sonar issues

--- src/main/java/com/wellsky/workflow/WorkflowApplication.java
+++ src/main/java/com/wellsky/workflow/WorkflowApplication.java
@@ -6,14 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WorkflowApplication {
 
-    public String PASSWORD = "SECRETPASSWORD";
-
     public static void main(String[] args) {
-        //Bad variable name
-        String SuperHorrible_VarName="";
-        String bad="";
-        //System out should be a critical
-        System.out.println(SuperHorrible_VarName + bad);
         SpringApplication.run(WorkflowApplication.class, args);
     }
 }

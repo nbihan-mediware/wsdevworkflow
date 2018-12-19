003d5e2d40e181907504cfc232daa675df6a7208
Nicolas Bihan <nicolas.bihan@wellsky.com>
2018-12-18 21:10:23 -0600

    Not so great code

--- src/test/java/com/wellsky/workflow/WorkflowApplicationTests.java
+++ src/test/java/com/wellsky/workflow/WorkflowApplicationTests.java
@@ -1,16 +1,29 @@
 package com.wellsky.workflow;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WorkflowApplicationTests {
 
+    @Autowired
+    ApplicationContext applicationContext;
+
     @Test
     public void contextLoads() {
+        System.out.println(applicationContext.getApplicationName());
     }
 
+    @Test
+    public void fakeMethodTest(){
+        String expected = "12";
+        String result = WorkflowApplication.fakeMethod(Integer.valueOf(expected));
+        Assert.assertEquals(expected, result);
+    }
 }
